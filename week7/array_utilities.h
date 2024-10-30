8f52baf703cf0ef2170b92712b9d99df8382b79e
Kaiden <jessanik@mcmaster.ca>
2024-10-30 13:05:59 -0400

    week7

--- week7/array_utilities.h
+++
@@ -1,4 +0,0 @@
-int find_index(int num, int n, int array[n]);
-int* find_pointer(int size, int a[size], int key);
-int count(int size, int a[size], int key);
-int count2d(int a, int b, int arr[a][b], int key);
